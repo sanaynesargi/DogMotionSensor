a8a77652490c8cc91ddce87f9241412c486a5fd0
Sanay Nesargi <sanaynesargi@gmail.com>
2021-01-21 22:53:12 -0600

    finished data set gathering and made utility for checking how many files are in directory

---
+++ counter.py
@@ -0,0 +1,30 @@
+import pygame
+
+
+def close_program()L
+
+def count_files():
+
+    pygame.init()
+
+    font = pygame.font.SysFont('comicsans', 60, True)
+    win = pygame.display.set_mode((450, 400))
+
+    run = True
+    while run:
+
+        win.fill((18, 52, 86))
+        length = len(os.listdir("data_set/sanay"))
+        text = font.render(str(length), True, (255, 255, 255))
+
+        win.blit(text, (195, 200))
+        pygame.display.update()
+        
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                run = False
+    
+    pygame.quit()
+
+count_files()
--- test_stream.py
+++ test_stream.py
@@ -1,42 +1,47 @@
+#/usr/bin/python3
+
 import cv2
 from time import ctime
 import os
 
+
 video = cv2.VideoCapture(r"rtsp://Atom:Atom1234@192.168.86.28/live", cv2.CAP_FFMPEG)
+person = 'sanay'
+                
 
+def main():
+    while True:
+        ret,img = video.read()
+        time = ctime()
+        count = len(os.listdir("data_set/sanay/"))
 
-while True:
-
-    ret,img = video.read()
-    time = ctime()
-        
-    if '24:25:10' in time:
-        with open('results.txt', 'a') as f:
-            f.write("Stream completed succesfully program ended at {}".format(time))
-        break
-
-    try:
-
-        if ret:
-            cv2.imshow('FRAME', img)
-        else:
-            with open('log.txt', 'a') as log:
-                t = ctime()
-                log.write(f"Stream froze at {t}\n")
+        if count == 10000:
+            print(f"\n\nGOAL REACHED\n\n")
             break
 
-        if (cv2.waitKey(1) & 0xFF == ord('q')) or not ret:
-            breakcv2.de
+        try:
+            if (cv2.waitKey(1) & 0xFF == ord('q')) or not ret:
+                break
+
+            if ret:
+                cv2.imshow('FRAME', img)
+                print(f"writing {time}.png to data_set/sanay/ COUNT: {count}")
+                cv2.imwrite(f'data_set/{person}/{time}.png', img)
+
+            else:
+                with open('log.txt', 'a') as log:
+                    t = ctime()
+                    log.write(f"Stream froze at {t}\n")
+                break
 
 
-    except KeyboardInterrupt:
-        break
+        except KeyboardInterrupt:
+            break
 
-cap.release()
-cv2.destroyAllWindows()
+    cap.release()
+    cv2.destroyAllWindows()
 
-if '24:25:10' in time:
-    with open('results.txt', 'a') as f:
-        f.write("Stream completed succesfully program ended at {}".format(time))
-else:
-    os.system("python3 test_stream.py")
+
+if __name__ == "__main__":
+    main()
+    
